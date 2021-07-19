6361adb864ec81148138eae8a68dc8c2afb3dbc9
Miguel <79226657+Los-had@users.noreply.github.com>
2021-07-19 23:20:50 +0000

    estilo e formatação

--- main.rb
+++ main.rb
@@ -4,12 +4,17 @@ txt.scan(/./)
 len = txt.length
 result = ""
 j = 0
+k = 0
 
 puts "-" * 55
-while j <= 25 do 
-  len.times do |i|
-    result += (txt[i].ord - j).chr
+while j <= 25 do
+  while k <= 25 do 
+    len.times do |i|
+      result += (txt[i].ord - j).chr
+    end
+    puts "[#{k}]: #{result}\n"
+    puts "-" * 55 + "\n"
+    j += 1
+    k += 1
   end
-  puts "#{result}\n\n"
-  j += 1
 end
